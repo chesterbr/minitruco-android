108e75feb02e722936aa0d00180f71b2e81fa5eb
Chester <cd@pobox.com>
2011-07-09 13:37:06 -0300

    Menus sobre e ajuda disponíveis o tempo todo (via BaseActivity) (fix commit)

---
+++ minitruco-android/src/me/chester/minitruco/android/BaseActivity.java
@@ -0,0 +1,76 @@
+package me.chester.minitruco.android;
+
+import me.chester.minitruco.R;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/*
+ * Copyright © 2005-2011 Carlos Duarte do Nascimento (Chester)
+ * cd@pobox.com
+ * 
+ * Este programa é um software livre; você pode redistribui-lo e/ou 
+ * modifica-lo dentro dos termos da Licença Pública Geral GNU como 
+ * publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+ * Licença.
+ *
+ * Este programa é distribuido na esperança que possa ser util, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO
+ * a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença
+ * Pública Geral GNU para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa, se não, escreva para a Fundação do Software
+ * Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Processa menus e diálogos comuns à tela de título (
+ * <code>TituloActivity</code>) e à tela de jogo (<code>TrucoActivity</code>).
+ * 
+ * @author chester
+ * 
+ */
+public abstract class BaseActivity extends Activity {
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.base, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuitem_ajuda:
+			mostraAlertBox(this.getString(R.string.titulo_ajuda), this
+					.getString(R.string.texto_ajuda));
+			return true;
+		case R.id.menuitem_sobre:
+			mostraAlertBox(this.getString(R.string.titulo_sobre), this
+					.getString(R.string.texto_sobre));
+			return true;
+		case R.id.menuitem_sair_titulo:
+		case R.id.menuitem_sair_truco:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	protected void mostraAlertBox(String titulo, String texto) {
+		new AlertDialog.Builder(this).setTitle(titulo).setMessage(
+				Html.fromHtml(texto)).setNeutralButton("Ok",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+					}
+				}).show();
+	}
+
+}
