e08dcbc0367cdbb0b4f129bcaa4ca6bb79fa70ef
Chester <cd@pobox.com>
2012-01-14 02:51:41 -0200

    Atualizando versão do BCF Ads

--- minitruco-android/src/me/chester/minitruco/android/Publicidade.java
+++ minitruco-android/src/me/chester/minitruco/android/Publicidade.java
@@ -1,39 +1,34 @@
 package me.chester.minitruco.android;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
-import com.bestcoolfungames.adsclient.BCFGAds;
-import com.bestcoolfungames.adsclient.BCFGAdsListener;
+import com.bcfg.adsclient.BCFAds;
+import com.bcfg.adsclient.ads.Popup;
+import com.bcfg.adsclient.ads.Popup.OnLoadListener;
 
 public class Publicidade {
 
 	private static final String BCFGADS_APP_ID = "4f075f1f044a0d0003012e0a";
 	private static boolean mPodeMostrar = false;
-	private static BCFGAds ads;
-	private static BCFGAdsListener listener = new BCFGAdsListener() {
-		public void adReceived() {
-			Log.w("MINITRUCO", "mostrar");
-			mPodeMostrar = true;
+	private static Popup popup;
+	private static OnLoadListener listener = new OnLoadListener() {
+
+		public void onAdNotReceived(LoadError error) {
+			Log.w("MINITRUCO", "nao mostrar: " + error);
+			mPodeMostrar = false;
 		}
 
-		public void adNotReceived() {
-			Log.w("MINITRUCO", "nao mostrar");
-			mPodeMostrar = false;
+		public void onAdReceived(Popup popup) {
+			Log.w("MINITRUCO", "mostrar");
+			Publicidade.popup = popup;
+			mPodeMostrar = true;
 		}
 	};
 
-	public static void inicializa(Context context) {
-
-		ads = new BCFGAds(context, listener, BCFGADS_APP_ID) {
-			@Override
-			public void showAd(Context c) {
-				// Does nothing. It is just to avoid the bizarre
-				// show-ad-out-of-nowhere behavior
-			}
-
-		};
-		ads.requestAd();
+	public static void inicializa(Activity activity) {
+		BCFAds.loadPopup(activity, BCFGADS_APP_ID, listener);
 	}
 
 	public static boolean podeMostrar() {
@@ -41,12 +36,12 @@ public class Publicidade {
 	}
 
 	public static String getMensagem() {
-		return mPodeMostrar ? ads.message() : "";
+		return mPodeMostrar ? popup.getMessage() : "";
 	}
 
 	public static void click() {
-		if (mPodeMostrar) {
-			ads.yesPressed();
+		if (mPodeMostrar && popup != null) {
+			popup.click();
 			mPodeMostrar = false;
 		}
 	}
