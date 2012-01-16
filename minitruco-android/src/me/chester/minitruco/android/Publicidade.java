9f21a65aab079edab668cac9b7d153c5334d2d61
Chester <cd@pobox.com>
2012-01-15 23:25:51 -0200

    Mudando versão da lib BCFG (sem incluir ainda)

--- minitruco-android/src/me/chester/minitruco/android/Publicidade.java
+++ minitruco-android/src/me/chester/minitruco/android/Publicidade.java
@@ -10,7 +10,8 @@ import com.bcfg.adsclient.ads.Popup.OnLoadListener;
 
 public class Publicidade {
 
-	private static final String BCFGADS_APP_ID = "4f075f1f044a0d0003012e0a";
+	private static final String BCFGADS_APP_ID_PRODUCTION = "4f075f1f044a0d0003012e0a";
+	private static final String BCFGADS_APP_ID_STAGING = "4f110f6e4824950003000005";
 	private static boolean mPodeMostrar = false;
 	private static Popup popup;
 	private static OnLoadListener listener = new OnLoadListener() {
@@ -28,7 +29,11 @@ public class Publicidade {
 	};
 
 	public static void inicializa(Activity activity) {
-		BCFAds.loadPopup(activity, BCFGADS_APP_ID, listener);
+		String id = BCFGADS_APP_ID_PRODUCTION;
+		if (BCFAds.isStagingMode()) {
+			id = BCFGADS_APP_ID_STAGING;
+		}
+		BCFAds.loadPopup(activity, id, listener);
 	}
 
 	public static boolean podeMostrar() {
