1b9edc4e8e6a5b9f242993574a17ddc05614f2ec
Carlos Duarte Do Nascimento <cd@pobox.com>
2023-05-16 15:36:12 -0400

    Expande um pouco o teste de Carta (que já bateu em algo relevante para Estrategia)

--- core/src/main/java/me/chester/minitruco/core/CartaTest.java
+++ core/src/main/java/me/chester/minitruco/core/CartaTest.java
@@ -1,6 +1,7 @@
 package me.chester.minitruco.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,33 @@ import org.junit.jupiter.api.Test;
 class CartaTest {
 
     @Test
+    @DisplayName("Pode ser instanciada por letra/naipe ou string")
+    void testCreate() {
+        Carta carta7p = new Carta("7p");
+        assertEquals('7', carta7p.getLetra());
+        assertEquals(Carta.NAIPE_PAUS, carta7p.getNaipe());
+
+        Carta carta3o = new Carta('3', Carta.NAIPE_OUROS);
+        assertEquals('3', carta3o.getLetra());
+        assertEquals(Carta.NAIPE_OUROS, carta3o.getNaipe());
+    }
+
+    @Test
     @DisplayName("Representa adequadamente como String")
     void testToString() {
         Carta c = new Carta('K', Carta.NAIPE_ESPADAS);
         assertEquals("Ke", c.toString());
     }
+
+    @Test
+    @DisplayName("É igual a outra carta se tiverem a mesma letra e naipe")
+    void testEquals() {
+        Carta cartaKe = new Carta("Ke");
+        assertEquals(new Carta("Ke"), cartaKe);
+        assertNotEquals(new Carta("Kc"), cartaKe);
+        assertNotEquals(new Carta("Ae"), cartaKe);
+    }
+
+    // TODO getValorTruco deveria suportar manilha velha
+    //      (mas tem que ver as consequencias para estrategias)
 }
