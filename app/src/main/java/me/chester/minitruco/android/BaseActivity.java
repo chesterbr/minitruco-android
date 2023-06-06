bcc096ad4644749e995b04d81e73996f3cb52db0
Carlos Duarte Do Nascimento <cd@pobox.com>
2023-06-06 15:21:36 -0400

    Move menus da tela de título para botões e remove action bar

--- app/src/main/java/me/chester/minitruco/android/BaseActivity.java
+++ app/src/main/java/me/chester/minitruco/android/BaseActivity.java
@@ -1,17 +1,9 @@
 package me.chester.minitruco.android;
 
 import android.app.AlertDialog;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.text.Html;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
-import me.chester.minitruco.R;
 
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright © 2005-2023 Carlos Duarte do Nascimento "Chester" <cd@pobox.com> */
@@ -21,48 +13,6 @@ import me.chester.minitruco.R;
  * <code>TituloActivity</code>) e à tela de jogo (<code>TrucoActivity</code>).
  */
 public abstract class BaseActivity extends AppCompatActivity {
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.base, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.menuitem_instrucoes:
-            mostraAlertBox(this.getString(R.string.titulo_instrucoes),
-                    this.getString(R.string.texto_instrucoes));
-            return true;
-        case R.id.menuitem_sobre:
-            SharedPreferences preferences = PreferenceManager
-                    .getDefaultSharedPreferences(this);
-            int partidas = preferences.getInt("statPartidas", 0);
-            int vitorias = preferences.getInt("statVitorias", 0);
-            int derrotas = preferences.getInt("statDerrotas", 0);
-            String versao;
-            try {
-                versao = getPackageManager()
-                        .getPackageInfo(getPackageName(), 0).versionName;
-            } catch (NameNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            String stats_versao = "Esta é a versão " + versao
-                    + " do jogo. Você já iniciou " + partidas
-                    + " partidas, ganhou " + vitorias + " e perdeu " + derrotas
-                    + ".<br/><br/>";
-            mostraAlertBox(this.getString(R.string.titulo_sobre), stats_versao
-                    + this.getString(R.string.texto_sobre));
-            return true;
-        case R.id.menuitem_sair_titulo:
-            finish();
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
 
     protected void mostraAlertBox(String titulo, String texto) {
         runOnUiThread(() -> {
