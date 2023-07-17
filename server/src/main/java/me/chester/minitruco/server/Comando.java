d2de8a694c1fbc83fcd526fe60669b0e0f9d3aa4
Carlos Duarte Do Nascimento <cd@pobox.com>
2023-07-17 18:28:39 -0400

    Move interpretação do comando para método próprio
    isso facilita os testes e deixa JogadorConectado mais limpo
    
    é preciso processar desconexão melhor

--- server/src/main/java/me/chester/minitruco/server/Comando.java
+++ server/src/main/java/me/chester/minitruco/server/Comando.java
@@ -3,6 +3,8 @@ package me.chester.minitruco.server;
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright © 2005-2023 Carlos Duarte do Nascimento "Chester" <cd@pobox.com> */
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Superclasse de todos os comandos que podem ser emitidos por um cliente.
  * <p>
@@ -26,5 +28,42 @@ public abstract class Comando {
      * @param j    Jogador que solicitou o comando
      */
     public abstract void executa(String[] args, JogadorConectado j);
+
+    /**
+     * Interpreta uma linha de comando e executa com a classe apropriada
+     *
+     * @param linha comando recebido do cliente (ex.: "N Carlos" para "meu nome é Carlos")
+     * @param j Jogador que solicitou o comando
+     * @see /docs/desenvolvimento.md#protocolo-de-comunicação-multiplayer
+     */
+    public static void interpreta(String linha, JogadorConectado j) {
+        // Quebra a solicitação em tokens
+        String[] args = linha.split(" ");
+        if (args.length == 0 || args[0].length() == 0) {
+            return;
+        }
+
+        // Encontra a implementação do comando solicitado e chama
+        if (args[0].length() != 1) {
+            return;
+        }
+        char comando = Character.toUpperCase(args[0].charAt(0));
+        try {
+            Comando c = (Comando) Class.forName(
+                    "me.chester.minitruco.server.Comando"
+                            + comando).getDeclaredConstructor().newInstance();
+            c.executa(args, j);
+        } catch (ClassNotFoundException e) {
+            j.println("X CI");
+        } catch (InstantiationException e) {
+            j.println("X CI");
+        } catch (IllegalAccessException e) {
+            j.println("X CI");
+        } catch (InvocationTargetException e) {
+            j.println("X CI");
+        } catch (NoSuchMethodException e) {
+            j.println("X CI");
+        }
+    }
 
 }
