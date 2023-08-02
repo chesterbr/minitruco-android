9696b2973a6778d6dbefc036aedce0cb06431a8f
Carlos Duarte Do Nascimento <cd@pobox.com>
2023-08-02 16:17:50 -0400

    Introduz um rascunho de teste de carga
    (que já revelou que eu tinha uma idéia errada do funcionamento de Threads no Java e talvez tenha quer rever, tipo, tudo)

---
+++ server/src/test/java/me/chester/minitruco/server/LoadTest.java
@@ -0,0 +1,47 @@
+package me.chester.minitruco.server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class LoadTest {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int porta = 6912;
+
+        int numConexoes = 4096;
+
+        for (int i = 0; i < numConexoes; i++) {
+            new Thread(() -> {
+                try {
+                    Socket socket = new Socket(host, porta);
+                    System.out.println("Conectado " + host + ":" + porta);
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                            socket.getOutputStream())), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(
+                            socket.getInputStream()));
+                    out.println("N robozinho");
+                    out.println("E PUB P");
+                    String linha = in.readLine();
+                    while ((linha = in.readLine()) != null) {
+                        System.out.println("Recebido: " + linha);
+                        if (linha.startsWith("K")) {
+                            out.println(linha);
+                            System.out.println("Mandou keepalive");
+                        } else if (linha.endsWith("1 PUB") && !linha.contains("bot")) {
+                            out.println("Q");
+                        }
+                    }
+
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println("Erro ao conectar em " + host + ":" + porta + ": " + e.getMessage());
+                }
+            }).start();
+        }
+    }
+}
