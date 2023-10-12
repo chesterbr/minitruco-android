2115f4c151d1aa16c66b64e7c31a63a2967682b0
Carlos Duarte Do Nascimento <cd@pobox.com>
2023-10-12 16:20:26 -0400

    Adiciona o py4j no projeto Java e disponibiliza a VM para ser chamada pelo Python

--- trainingenv/src/main/java/me/chester/minitruco/trainingenv/TrainingEnv.java
+++ trainingenv/src/main/java/me/chester/minitruco/trainingenv/TrainingEnv.java
@@ -1,8 +1,21 @@
 package me.chester.minitruco.trainingenv;
 
+import py4j.GatewayServer;
+
 public class TrainingEnv {
 
+    public static String init() {
+        return "Java diz: Olá, mundo!";
+    }
+
+    public int addition(int first, int second) {
+      return first + second;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        TrainingEnv app = new TrainingEnv();
+        // app is now the gateway.entry_point
+        GatewayServer server = new GatewayServer(app);
+        server.start();
     }
 }
