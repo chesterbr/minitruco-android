809153d9b3583ad984511e241f61179e20967bd2
Carlos Duarte Do Nascimento <cd@pobox.com>
2024-01-10 19:24:48 -0500

    Testa troca fora da vez
    (e coloca os asserts num lugar mais facil de ver)

--- core/src/test/java/me/chester/minitruco/core/PartidaLocalTest.java
+++ core/src/test/java/me/chester/minitruco/core/PartidaLocalTest.java
@@ -19,9 +19,7 @@ class PartidaLocalTest {
                 if (j == this) {
                     PartidaLocal p = (PartidaLocal) partida;
                     int posicao = getPosicao();
-                    assertNotEquals(JogadorBot.class, p.getJogador(posicao).getClass());
                     p.trocaUsuarioPorBot(this);
-                    assertEquals(JogadorBot.class, p.getJogador(posicao).getClass());
                     ((JogadorBot)p.getJogador(posicao)).setFingeQuePensa(false);
                 }
             }
@@ -31,10 +29,39 @@ class PartidaLocalTest {
         partida.adiciona(new JogadorDeTeste());
         partida.adiciona(jogadorTrocadoNaVez);
         partida.adiciona(new JogadorDeTeste());
+        assertNotEquals(JogadorBot.class, partida.getJogador(3).getClass());
         (new Thread(partida)).start();
         while (!partida.finalizada) {
             sleep(100);
         }
+        assertEquals(JogadorBot.class, partida.getJogador(3).getClass());
+    }
+
+    @Test
+    @Timeout(30)
+    void testTrocaUsuarioPorBotSubstituiEContinuaJogoForaDaVezDoUsuario() throws InterruptedException {
+        Jogador jogadorTrocadoForaDaVez = new JogadorDeTeste() {
+            @Override
+            public void vez(Jogador j, boolean podeFechada) {
+                if (j != this) {
+                    PartidaLocal p = (PartidaLocal) partida;
+                    int posicao = getPosicao();
+                    p.trocaUsuarioPorBot(this);
+                    ((JogadorBot)p.getJogador(posicao)).setFingeQuePensa(false);
+                }
+            }
+        };
+        PartidaLocal partida = new PartidaLocal(false, false, "P");
+        partida.adiciona(new JogadorDeTeste());
+        partida.adiciona(new JogadorDeTeste());
+        partida.adiciona(jogadorTrocadoForaDaVez);
+        partida.adiciona(new JogadorDeTeste());
+        assertNotEquals(JogadorBot.class, partida.getJogador(3).getClass());
+        (new Thread(partida)).start();
+        while (!partida.finalizada) {
+            sleep(100);
+        }
+        assertEquals(JogadorBot.class, partida.getJogador(3).getClass());
     }
 
 }
