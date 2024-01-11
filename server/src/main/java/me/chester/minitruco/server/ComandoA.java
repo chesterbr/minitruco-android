580ed56ded59469d2f335c04f77401de3b73ddd1
Carlos Duarte Do Nascimento <cd@pobox.com>
2024-01-11 17:47:39 -0500

    Se um usuário abandona sala pública com jogo em andamento, troca ele por bot
    (o básico funciona, mas ainda é preciso lidar com o aftermatch)

--- server/src/main/java/me/chester/minitruco/server/ComandoA.java
+++ server/src/main/java/me/chester/minitruco/server/ComandoA.java
@@ -3,6 +3,8 @@ package me.chester.minitruco.server;
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright © 2005-2023 Carlos Duarte do Nascimento "Chester" <cd@pobox.com> */
 
+import me.chester.minitruco.core.Jogador;
+
 /**
  * Informa ao servidor que o jogador abandonou a partida.
  */
@@ -17,7 +19,17 @@ public class ComandoA extends Comando {
         if (sala.getPartida() == null) {
             sala.mandaInfoParaTodos();
         } else {
-            sala.getPartida().abandona(j.getPosicao());
+            if (sala.isPublica()) {
+                sala.getPartida().trocaPorBot(j);
+                for (int i = 1; i <= 4; i++) {
+                    Jogador outroJogador = sala.getPartida().getJogador(i);
+                    if (outroJogador instanceof JogadorConectado) {
+                        ((JogadorConectado) outroJogador).println("! T " + j.getNome() + " saiu; bot🤖 entrou no lugar.");
+                    }
+                }
+            } else {
+                sala.getPartida().abandona(j.getPosicao());
+            }
         }
     }
 }
--- server/src/test/java/me/chester/minitruco/server/ComandoATest.java
+++ server/src/test/java/me/chester/minitruco/server/ComandoATest.java
@@ -1,5 +1,6 @@
 package me.chester.minitruco.server;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -13,21 +14,25 @@ import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import me.chester.minitruco.core.JogadorBot;
+
 class ComandoATest {
 
-    private static JogadorConectado j1, j2;
+    private static JogadorConectado j1, j2, j3;
 
     @BeforeEach
     void setUp() {
         j1 = spy(new JogadorConectado(null));
         j2 = spy(new JogadorConectado(null));
+        j3 = spy(new JogadorConectado(null));
         doNothing().when(j1).println(any());
         doNothing().when(j2).println(any());
+        doNothing().when(j3).println(any());
     }
 
     @Test
-    void testAbortaEncerraPartidaENotificaSemAtualizarSala() {
-        Sala s = new Sala(true, "P");
+    void testAbortaSalaPrivadaComPartidaEncerraENotificaSemAtualizarSala() {
+        Sala s = new Sala(false, "P");
         s.adiciona(j1);
         s.adiciona(j2);
         s.iniciaPartida(j1);
@@ -39,6 +44,49 @@ class ComandoATest {
         verify(j2).println(argThat((String msg) -> msg.matches("A 2 .+")));
         verify(j1, never()).println(argThat((String msg) -> msg.startsWith("I ")));
     }
+
+    @Test
+    void testAbortaSalaPublicaComPartidaNaoNotificaSaidaNemAtualizarSala() {
+        Sala s = new Sala(true, "P");
+        s.adiciona(j1);
+        s.adiciona(j2);
+        s.iniciaPartida(j1);
+        assertNotNull(s.getPartida());
+
+        Comando.interpreta("A", j2);
+        verify(j1, never()).println(argThat((String msg) -> msg.startsWith("I ")));
+        verify(j1, never()).println(argThat((String msg) -> msg.startsWith("A ")));
+    }
+
+    @Test
+    void testAbortaSalaPublicaComPartidaInformaTrocaParaOutrosJogadores() {
+        Sala s = new Sala(true, "P");
+        s.adiciona(j1);
+        s.adiciona(j2);
+        s.adiciona(j3);
+        j2.setNome("joselito");
+        s.iniciaPartida(j1);
+
+        Comando.interpreta("A", j2);
+        verify(j1).println(argThat((String msg) -> msg.matches("! T .*joselito.*")));
+        verify(j3).println(argThat((String msg) -> msg.matches("! T .*joselito.*")));
+    }
+
+    @Test
+    void testAbortaSalaPublicaTrocaUsuarioPorBot() {
+        Sala s = new Sala(true, "P");
+        s.adiciona(j1);
+        s.adiciona(j2);
+        s.iniciaPartida(j1);
+        assertNotNull(s.getPartida());
+
+        Comando.interpreta("A", j2);
+        assertNotNull(s.getPartida());
+        // TODO o que eu quero fazer na *sala*?
+//        assertEquals(JogadorBot.class, s.getJogador(2).getClass());
+        assertEquals(JogadorBot.class, s.getPartida().getJogador(2).getClass());
+    }
+
 
     @Test
     void testAbortaEmSalaMasForaDeJogoAtualizaSalaParaOutrosJogadores() {
